2f9e49a90b9c34cdbc2bbadc253c156177751372
赵佳福 <stern@pc.local>
2018-11-06 11:04:55 +0800

    Initial Commit

---
+++ C++_L/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++_L
+//
+//  Created by Stern on 2018/11/6.
+//  Copyright © 2018 Stern. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
