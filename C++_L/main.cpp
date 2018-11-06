1a4e7a07a7101de1d3170878197319c86dc7ca1d
赵佳福 <stern@pc.local>
2018-11-06 15:57:13 +0800

    initial commit

--- C++_L/main.cpp
+++ C++_L/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    cout <<"I am here!\n";
     return 0;
 }
